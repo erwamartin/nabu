49a2050078fd619cc98933cbb4181ee5d83cf2b3
Erwan Martin <contact@erwanmartin.fr>
2014-12-02 19:19:00 +0100

    update User model adding additional fields

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141202181305) do
+ActiveRecord::Schema.define(version: 20141202181616) do
 
   create_table "posts", force: true do |t|
     t.integer  "user_id"
@@ -35,6 +35,9 @@ ActiveRecord::Schema.define(version: 20141202181305) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "username"
+    t.string   "description"
+    t.string   "picture"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
