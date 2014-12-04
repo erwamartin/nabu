e2d872dbb64d06a0e7ae8ad387cafb01d27ef85d
Erwan Martin <contact@erwanmartin.fr>
2014-12-04 10:27:08 +0100

    edit User model adding validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,23 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
+  validates :username,  presence: true, uniqueness: true
+
+  def create
+    User.create(params[:user])
+  end
+
+  def update
+    #user = current_account.people.find(params[:id])
+    #user.update!(user_params)
+    #redirect_to user
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:username, :email, :description, :picture, :password)
+    end
+
 end
