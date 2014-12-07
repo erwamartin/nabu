af28e8ab8da9e2db001cbd31ce7e5da59692d3d3
annesophievalleray <annesophie.valleray@gmail.com>
2014-12-07 15:23:58 +0100

    profile picture upload -- new: migration+gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,15 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
   validates :username,  presence: true, uniqueness: true
 
+  has_attached_file :picture, :styles => { :small => "300x300>", :thumb => "100x100#"  },
+  				  :default_url => "/images/:style/missing.png",
+                  :url  => "/assets/pictures/:id/:style/:basename.:extension",
+                  :path => ":rails_root/public/assets/pictures/:id/:style/:basename.:extension"
+
+  validates_attachment_presence :picture
+  validates_attachment_size :picture, :less_than => 5.megabytes
+  validates_attachment_content_type :picture, :content_type => ['image/jpeg', 'image/png']
+
   def create
     User.create(params[:user])
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141206160618) do
+ActiveRecord::Schema.define(version: 20141207135608) do
 
   create_table "bookmarks", force: true do |t|
     t.integer  "user_id"
@@ -60,6 +60,10 @@ ActiveRecord::Schema.define(version: 20141206160618) do
     t.string   "username"
     t.string   "description"
     t.string   "picture"
+    t.string   "picture_file_name"
+    t.string   "picture_content_type"
+    t.integer  "picture_file_size"
+    t.datetime "picture_updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
