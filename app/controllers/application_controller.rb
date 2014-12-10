6454eac368f6bb8214c9e11f64f04b2a76076b6f
Erwan Martin <contact@erwanmartin.fr>
2014-12-10 01:06:33 +0100

    move variables declaration from layout filter to before filter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,19 +2,23 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   helper_method :resource, :resource_name, :devise_mapping
+  before_filter :set_data
   layout :get_layout
 
   def get_layout
     if current_user.nil?
       "landing"
     else 
-      @current_user = current_user
-      @target_user = current_user
-      @followings = current_user.following
-      @followers = current_user.followers
-      @follows_current_user = current_user.following
       "application"
     end
+  end
+
+  def set_data
+    @current_user = current_user
+    @target_user = current_user
+    @followings = current_user.following
+    @followers = current_user.followers
+    @follows_current_user = current_user.following
   end
 
   def resource_name
@@ -27,6 +31,32 @@ class ApplicationController < ActionController::Base
 
   def devise_mapping
     @devise_mapping ||= Devise.mappings[:user]
+  end
+
+  # Tools methods
+
+  def get_followings_array_id
+    following_record = Relation.where(follower_id: current_user.id)
+    followings = []
+
+    following_record.each do |following|
+      followings = followings + [following.following_id]
+    end
+
+    @followings = followings
+
+  end
+
+  def get_followers_array_id
+    follower_record = Relation.where(following_id: current_user.id)
+    followers = []
+
+    follower_record.each do |follower|
+      followers = followers + [follower.follower_id]
+    end
+
+    @followers = followers
+
   end
 
   
