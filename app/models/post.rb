f39a4354b83647acca40a9e16cc99502b11c8d85
Erwan Martin <contact@erwanmartin.fr>
2014-12-12 11:51:45 +0100

    secure post destroy

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,10 +3,10 @@ class Post < ActiveRecord::Base
   belongs_to :user
 
   has_many :bookmarks
-  has_many :bookmarks_users, :through=> :bookmarks, :class_name => 'User', :foreign_key => 'user_id', :source => :user
+  has_many :bookmarks_users, :through=> :bookmarks, :class_name => 'User', :foreign_key => 'user_id', :source => :user, :dependent => :destroy
 
   has_many :reposts
-  has_many :reposts_users, :through=> :reposts, :class_name => 'User', :foreign_key => 'user_id', :source => :user
+  has_many :reposts_users, :through=> :reposts, :class_name => 'User', :foreign_key => 'user_id', :source => :user, :dependent => :destroy
 
   validates :user, presence: true
   validates :url, presence: true, format: { with: /(^$)|(^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$)/ix , on: :create }
