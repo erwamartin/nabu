7c78c3ed3ec42444f6df0a165883bc7c543512a1
Lucie Reitz <lucie.reitz@gmail.com>
2014-12-12 15:51:27 +0100

    fix translation missing

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -1,5 +1,13 @@
 module DeviseHelper
   def devise_error_messages!
-    resource.errors.full_messages.map { |msg| content_tag(:p, msg) }.join.html_safe
+       messages = resource.errors.full_messages.map { |msg| content_tag(:p, msg) }.join
+
+   html = <<-HTML
+   <div class="alert-error">
+     #{messages}
+   </div>
+   HTML
+
+   html.html_safe
   end
 end
