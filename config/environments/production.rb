d760d740d2b9146d007ba637565b04faa9e49c53
Erwan Martin <contact@erwanmartin.fr>
2014-12-12 23:30:37 +0100

    set Amazon S3 to production only

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,4 +82,8 @@ Rails.application.configure do
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
   }
+
+  Paperclip::Attachment.default_options[:url] = 'http://nabu-rails.s3.amazonaws.com'
+  Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
+  Paperclip::Attachment.default_options[:s3_host_name] = 's3-eu-central-1.amazonaws.com'
 end
